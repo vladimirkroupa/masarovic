27f3bb6f04d4f7e9fca8b20a853b0f951a679a44
Vladimír Kroupa <vladimirkroupa@gmail.com>
2023-01-27 16:35:18 +0100

    Do not write header between strains

--- processor.py
+++ processor.py
@@ -11,19 +11,17 @@ def convert_dataframe(df: DataFrame) -> DataFrame:
     strain_col = sheet.strains(df)
     strain_cnt = len(strain_col)
 
-    # first compound block
-    new_strain_col = strain_col
-    new_compound_no_col = itertools.repeat(1, strain_cnt)
-    new_abundance_col = sheet.compound_abundance(1, df)
+    new_strain_col = []
+    new_compound_no_col = []
+    new_abundance_col = []
 
-    # remaining compound blocks
-    for comp_no in range(2, compound_cnt + 1):
+    for comp_no in range(1, compound_cnt + 1):
         compound_no_col = itertools.repeat(comp_no, strain_cnt)
         abundance_col = sheet.compound_abundance(comp_no, df)
 
-        new_strain_col = itertools.chain(new_strain_col, ['Strain'], strain_col)
-        new_compound_no_col = itertools.chain(new_compound_no_col, [None], compound_no_col)
-        new_abundance_col = itertools.chain(new_abundance_col, [None], abundance_col)
+        new_strain_col = itertools.chain(new_strain_col, strain_col)
+        new_compound_no_col = itertools.chain(new_compound_no_col, compound_no_col)
+        new_abundance_col = itertools.chain(new_abundance_col, abundance_col)
 
     strain_compound_abundance = zip(new_strain_col, new_compound_no_col, new_abundance_col)
     return DataFrame(strain_compound_abundance, columns=['Strain', 'Compound', 'Abundance'])
